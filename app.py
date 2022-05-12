6705815ff06bcb7c0658d7452789efae34d67fa8
anakin <shridharkulkarni3.14@gmail.com>
2022-05-12 23:12:24 +0530

    some update

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from resources.item import Item, ItemRegister
 from resources.user import UserRegister
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "sid"
 api = Api(app)
 
@@ -19,4 +21,6 @@ api.add_Resource(UserRegister, "/register")
 
 
 if __name__ == "__main__":
+    from db import db
+    db.init_app(app)
     app.run(debug=True) 
